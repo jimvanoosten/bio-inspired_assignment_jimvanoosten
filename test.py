1df45866478ff04a0f81b106e09d9c8818206ae8
Jim van Oosten <j.a.h.vanoosten@student.tudelft.nl>
2023-06-16 16:49:14 +0200

    test

--- test.py
+++ test.py
@@ -1,17 +1,30 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
+import gymnasium as gym
 
-x = np.random.rand(10, 10)
-y = pd.DataFrame(x)
+environment_name = "LunarLander-v2"
 
-xpoints = np.array([0, 6])
-ypoints = np.array([0, 250])
-print(xpoints)
-print(ypoints)
+env = gym.make(environment_name, render_mode="human")
+episodes = 10
+for episode in range(1, episodes+1):
+    state = env.reset()
+    done = False
+    score = 0
 
-plt.plot(xpoints, ypoints)
-plt.show()
+    while not done:
+        env.render()
+        action = env.action_space.sample()
+        n_state, reward, done, info, a = env.step(action)
+        score += reward
+    print('Episode:{} Score:{}'.format(episode, score))
+env.close()
 
-plt.plot(x)
-plt.show()
+# env = gym.make(environment_name, render_mode="human")
+# observation, info = env.reset(seed=42)
+# for _ in range(1000):
+#    action = env.action_space.sample()  # this is where you would insert your policy
+#    observation, reward, terminated, truncated, info = env.step(action)
+#
+#    if terminated or truncated:
+#       observation, info = env.reset()
+# env.close()
+
+
