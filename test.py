41ff1abae77715f9f87f56aeadd723be980bef09
jimvanoosten <vanoostenjim@gmail.com>
2023-06-20 17:54:40 +0200

    notebook begin

--- test.py
+++ test.py
@@ -1,23 +1,25 @@
-import gymnasium as gym
-
-environment_name = "LunarLander-v2"
-
-# env = gym.make(environment_name, render_mode="human")
+# import gymnasium as gym
+#
+# env = gym.make("LunarLander-v2", render_mode="human")
+#
 # episodes = 10
-# for episode in range(1, episodes+1):
-#     state = env.reset()
-#     done = False
+# for episode in range(1, episodes + 1):
+#     observation, info = env.reset(seed=42)
+#     terminated = False
+#     truncated = False
 #     score = 0
 #
-#     while not done:
+#     while not terminated or truncated:
 #         env.render()
-#         action = env.action_space.sample()
-#         n_state, reward, done, info, a = env.step(action)
+#         action = env.action_space.sample()  # this is where you would insert your policy
+#         observation, reward, terminated, truncated, info = env.step(action)
 #         score += reward
 #     print('Episode:{} Score:{}'.format(episode, score))
+#
 # env.close()
 
-env = gym.make(environment_name, render_mode="human")
+import gymnasium as gym
+env = gym.make("LunarLander-v2", render_mode="human")
 observation, info = env.reset(seed=42)
 for _ in range(1000):
    action = env.action_space.sample()  # this is where you would insert your policy
